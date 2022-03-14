457fbd6d53bf52ea646f2c28dafab6ad70f232e1
佐々木晨朔 <dawn.beginning.lope@gmail.com>
2022-03-15 08:29:54 +0900

    Finished Gatsby Tutorial Part 7

--- src/pages/blog/{mdx.slug}.tsx
+++ src/pages/blog/{mdx.slug}.tsx
@@ -2,11 +2,16 @@ import * as React from "react";
 import { graphql } from "gatsby";
 import Layout from "../../components/layout";
 import { MDXRenderer } from "gatsby-plugin-mdx";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 const BlogPost = ({ data }: any) => {
+  const image: any = getImage(data.mdx.frontmatter.hero_image);
+  console.log(image);
+
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
+      <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt} />
       <MDXRenderer>{data.mdx.body}</MDXRenderer>
     </Layout>
   );
@@ -18,6 +23,14 @@ export const query = graphql`
       frontmatter {
         title
         date(formatString: "MMMM D, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
       body
     }
